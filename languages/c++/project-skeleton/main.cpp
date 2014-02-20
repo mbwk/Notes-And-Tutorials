2121cd03dc066f90bebda53984a3a356152550d4
mbwk <mbwk@users.noreply.github.com>
2014-02-20 16:38:53 +0000

    Added notes on types && pointers in C++, as well as a skeleton C++ project that should work on any Unix-like

---
+++ languages/c++/project-skeleton/main.cpp
@@ -0,0 +1,29 @@
+/**
+ *  Problem with this program? Let me know.
+ *
+ *  Author:   Karl M. B.
+ *  Github:   mbwk
+ *  Twitter:  @mbwkarl
+ *  Email:    bi4ka3er3@gmail.com
+ *  XMPP:     miyabe@xmpp.jp
+ *  Website:  www.mbwkarl.com
+ */
+
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  std::cout << "  Hello World!\n";
+  std::cout << "  ============\n";
+  std::cout << "# of args: " << argc - 1 << '\n';
+  if (argc > 2) {
+    std::cout << "arg list:\n";
+    for (int i = 1; i < argc; ++i) {
+      std::cout << '\t' << i << ".) " << argv[i] << '\n';
+    }
+  }
+  std::cout << "  ============\n";
+  std::cout << "  End of test.\n" << std::endl;
+  return 0;
+}
+
